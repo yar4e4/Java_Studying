7e1b67044073278ed960c4feb1043add81ef3f41
Yar4e <yarche.off@gmail.com>
2024-04-14 20:04:10 +0300

    New Exercise

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -2,7 +2,7 @@
 //
 //Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
 
-public class Program {
+public class Exercise1 {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 0; i < 1000; i++) {
