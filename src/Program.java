8f5e282a9735ab3537bd86f4e232d2907a737ce3
Yar4e <yarche.off@gmail.com>
2024-04-14 19:35:15 +0300

    Exercise 1

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,11 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.print(sum);
     }
 }
