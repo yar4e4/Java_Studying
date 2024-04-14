d3e6e4506ed488bb0191c6806c3291773ccc7016
Yar4e <167015075+yar4e4@users.noreply.github.com>
2024-04-14 19:38:02 +0300

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,7 @@
+//Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+//
+//Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+
 public class Program {
     public static void main(String[] args) {
         int sum = 0;
@@ -9,3 +13,4 @@ public class Program {
         System.out.print(sum);
     }
 }
+
